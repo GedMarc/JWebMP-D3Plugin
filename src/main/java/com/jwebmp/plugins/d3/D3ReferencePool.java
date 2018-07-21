0043ee7a5fa3671b4d3d98d60815c7c9bfa983cf
GedMarc <ged_marc@hotmail.com>
2018-07-22 01:28:16 +0200

    JDK 10 and Builds

--- src/main/java/com/jwebmp/plugins/d3/D3ReferencePool.java
+++ src/main/java/com/jwebmp/plugins/d3/D3ReferencePool.java
@@ -14,9 +14,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.jwebmp.components.d3;
+package com.jwebmp.plugins.d3;
 
-import com.jwebmp.base.references.JavascriptReference;
+import com.jwebmp.core.base.references.JavascriptReference;
 
 /**
  * @author Marc Magon
