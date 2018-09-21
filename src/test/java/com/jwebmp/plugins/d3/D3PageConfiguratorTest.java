631a7ce74168fd5ecedc5dccccea6a9e9c56898e
GedMarc <ged_marc@hotmail.com>
2018-09-21 17:37:57 +0200

    BTM Active Per Unit

---
+++ src/test/java/com/jwebmp/plugins/d3/D3PageConfiguratorTest.java
@@ -0,0 +1,15 @@
+package com.jwebmp.plugins.d3;
+
+import com.jwebmp.core.Page;
+import org.junit.jupiter.api.Test;
+
+public class D3PageConfiguratorTest
+{
+
+	@Test
+	public void isEnabled()
+	{
+		Page p = new Page();
+		System.out.println(p.toString(true));
+	}
+}
