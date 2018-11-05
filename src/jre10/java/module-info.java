ed967dc2df505fa2b0576dac91c0d66ca9eaf997
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:40:38 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,7 +1,10 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.d3.D3PageConfigurator;
+import com.jwebmp.plugins.d3.implementations.D3ExclusionsModule;
 
-module com.jwebmp.components.d3 {
+module com.jwebmp.plugins.d3 {
 	exports com.jwebmp.plugins.d3;
 
 	requires com.jwebmp.core;
@@ -9,7 +12,11 @@ module com.jwebmp.components.d3 {
 	requires com.fasterxml.jackson.annotation;
 
 	requires java.validation;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with D3PageConfigurator;
+
+	provides IGuiceScanModuleExclusions with D3ExclusionsModule;
+	provides IGuiceScanJarExclusions with D3ExclusionsModule;
 
 }
