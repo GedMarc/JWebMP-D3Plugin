46916cc02be95ce5742d09026664292b4a140b68
GedMarc <ged_marc@hotmail.com>
2020-11-12 01:19:54 +0200

    Jakarta Namespace - JLink building

--- src/main/java/com/jwebmp/plugins/d3/implementations/D3ExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/d3/implementations/D3ExclusionsModule.java
@@ -2,7 +2,7 @@ package com.jwebmp.plugins.d3.implementations;
 
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
-import javax.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotNull;
 import java.util.HashSet;
 import java.util.Set;
 
