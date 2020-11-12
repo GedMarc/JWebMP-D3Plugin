ecfa1f08c5d48d9d170043d69cdf0f90a13c2dc6
GedMarc <ged_marc@hotmail.com>
2020-11-13 00:40:26 +0200

    module name java.validation to jakarta.validation

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -4,7 +4,7 @@ module com.jwebmp.plugins.d3 {
 	requires com.jwebmp.core;
 	requires com.guicedee.logmaster;
 
-	requires java.validation;
+	requires jakarta.validation;
 	requires com.guicedee.guicedinjection;
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.d3.D3PageConfigurator;
